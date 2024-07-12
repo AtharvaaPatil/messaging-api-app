24dcd2d48aae38f5ad49a9750f0f36f4839a4a2c
AtharvaaPatil <atharvapatil0052@gmail.com>
2024-07-12 14:27:48 +0530

    Second Commit

---
+++ src/main/java/com/training/messaging/MessagingApiAppApplication.java
@@ -0,0 +1,19 @@
+package com.training.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApiAppApplication {
+	
+	public static String greeting(){
+		return "Hi Oracle!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApiAppApplication.class, args);
+		System.out.println(greeting());
+		
+	}
+
+}
---
+++ src/test/java/com/training/messaging/MessagingApiAppApplicationTests.java
@@ -0,0 +1,19 @@
+package com.training.messaging;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(120,100);
+		
+	}
+
+}
