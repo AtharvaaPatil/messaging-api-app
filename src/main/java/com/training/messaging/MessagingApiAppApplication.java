1a15c297a6d61f5502b902c775451b51e288883f
AtharvaaPatil <atharvapatil0052@gmail.com>
2024-07-12 15:11:45 +0530

    Greeting Function added

--- src/main/java/com/training/messaging/MessagingApiAppApplication.java
+++ src/main/java/com/training/messaging/MessagingApiAppApplication.java
@@ -2,6 +2,7 @@ package com.training.messaging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MessagingApiAppApplication {
@@ -9,11 +10,17 @@ public class MessagingApiAppApplication {
 	public static String greeting(){
 		return "Hi Oracle!";
 	}
+	
+	@GetMapping("/happyweekend")
+	public String weekend() {
+		return "Happy Weekend Everyone";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApiAppApplication.class, args);
 		System.out.println(greeting());
 		System.out.println("This app has no testing");
+		
 		
 	}
 
